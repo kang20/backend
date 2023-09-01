b19f4fc85bf76c855fb0548a36b8e308bbee35b4
Kkang <aorl2313@naver.com>
2023-09-01 13:38:18 +0900

    update memoryrepositorytest + complete

--- src/main/java/com/example/aiwebservice/repository/MemorymemberRepository.java
+++ src/main/java/com/example/aiwebservice/repository/MemorymemberRepository.java
@@ -32,6 +32,10 @@ public class MemorymemberRepository implements memberRepository{
                 .findAny();
     }
 
+    public void Clear() {
+        db.clear();
+    }
+
 
 
 
---
+++ src/test/java/com/example/aiwebservice/repository/memoryRepositoryTest.java
@@ -0,0 +1,40 @@
+package com.example.aiwebservice.repository;
+
+import com.example.aiwebservice.data.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+public class memoryRepositoryTest {
+    private MemorymemberRepository repository = new MemorymemberRepository();
+
+    @AfterEach
+    public void aftereach() {
+        repository.Clear();
+    }
+    @Test
+    public void 저장() {
+        Member member = new Member();
+        member.setName("name");
+        member.setMemberPassword("password");
+        member.setMemberId("Id");
+        repository.save(member);
+        Assertions.assertThat(repository.findbyid("Id").get())
+                .isEqualTo(member);
+    }
+
+
+    @Test
+    public void 조회_테스트() {
+        Member member = new Member();
+        member.setName("name");
+        member.setMemberPassword("password");
+        member.setMemberId("Id");
+        repository.save(member);
+        Assertions.assertThat(repository.findbyid("Id").get())
+                .isEqualTo(repository.findbypassword("password").get());
+
+    }
+
+
+}
